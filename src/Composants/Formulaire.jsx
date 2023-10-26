bc37da9405d48404cb656a5d2a8f1101c7e603ef
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-26 12:26:24 +0000

    first commit React

---
+++ src/Composants/Formulaire.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './FichierCSS/Formulaire.css'
+
+function Formulaire() {
+  return (
+    <body>
+        <form action="">
+            
+        </form>
+        <div>
+
+        </div>
+        <div class ="form">
+            <h1>CRUD OPERATIONS</h1>
+            <h2>SIGN IN</h2>
+            <p>Enter your credentials to access to your account</p>
+            <label htmlFor="email">Email</label>
+        </div>
+    </body>
+    
+  )
+}
+
+export default Formulaire
