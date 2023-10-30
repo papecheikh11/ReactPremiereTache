a5cf2df6e6280bb581632c1ce2a3e9e0854ea3c3
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-30 11:24:38 +0000

    modifier react

--- src/Composants/Formulaire.jsx
+++ src/Composants/Formulaire.jsx
@@ -4,18 +4,24 @@ import './FichierCSS/Formulaire.css'
 function Formulaire() {
   return (
     <body>
-        <form action="">
-            
-        </form>
-        <div>
+        <form action="" class ="form">
 
-        </div>
-        <div class ="form">
-            <h1>CRUD OPERATIONS</h1>
+        <div class= "titleForm">
+            <h1  class ="monTitre">CRUD OPERATIONS</h1>
             <h2>SIGN IN</h2>
             <p>Enter your credentials to access to your account</p>
-            <label htmlFor="email">Email</label>
         </div>
+        <div class="bodyForm">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder='Enter your email'/>
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder='Enter your password' />
+
+            <button>SIGN IN</button>
+            <p>Forgot your password? <a href="#">Reset password</a></p>
+
+        </div>
+        </form>
     </body>
     
   )
